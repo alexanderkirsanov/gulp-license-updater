43ad4ac91b2bddaeb0a1f269651a517cd9dd757d
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2016-01-24 00:19:58 +0300

    initial plugin implementation

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-var through = require('through2');    // npm install --save through2
+var through = require('through2');
+var gutil = require('gulp-util');
+var extend = require('object-assign');
 
-module.exports = function() {
-    return through.obj(function(file, encoding, callback) {
-        callback(null, doSomethingWithTheFile(file));
-    });
+module.exports = function (config, license)
+{
+    var getFileName = function (file)
+    {
+        var filename;
+        if (typeof file === 'string')
+        {
+            filename = file;
+        }
+        else if (typeof file.path === 'string')
+        {
+            filename = path.basename(file.path);
+        }
+        else
+        {
+            filename = '';
+        }
+        return filename;
+    };
+    var check = function(){
+
+    };
+    var processFile = function (file, encoding, callback)
+    {
+        var filename = getFileName(file);
+        var template = gutil.template(license, extend({
+            file: file, filename: filename
+        }, config));
+
+        if (config.check)
+        {
+            this.emit.bind(this, 'error');
+        }
+        if (config.format)
+        {
+
+        }
+        if (config.remove)
+        {
+
+
+        }
+    };
+    return through.obj(processFile);
 };
