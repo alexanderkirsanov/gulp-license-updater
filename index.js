7e4a6ac50d5334e6d811c27e8afb5c552800e153
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2016-01-24 23:01:20 +0300

    match method has been implemented

--- index.js
+++ index.js
@@ -2,48 +2,69 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var extend = require('object-assign');
 
-module.exports = function (config, license)
-{
-    var getFileName = function (file)
-    {
+module.exports = function (config, license) {
+    function getFileName(file) {
         var filename;
-        if (typeof file === 'string')
-        {
+        if (typeof file === 'string') {
             filename = file;
         }
-        else if (typeof file.path === 'string')
-        {
+        else if (typeof file.path === 'string') {
             filename = path.basename(file.path);
         }
-        else
-        {
+        else {
             filename = '';
         }
         return filename;
-    };
-    var check = function(){
+    }
 
-    };
-    var processFile = function (file, encoding, callback)
-    {
+    function matchStrings(template, str) {
+        if (template === undefined || str === undefined) {
+            return 0;
+        }
+        var maxLength = Math.max(template.length, str.length);
+        if (maxLength === 0) {
+            return 1;
+        }
+        var matchCount = new Array(maxLength).reduce(function (prev, curr, index) {
+            if (template.charAt(index) === str.charAt(index)) {
+                return ++prev;
+            } else {
+                return prev;
+            }
+        }, 0);
+        return matchCount / maxLength;
+    }
+
+    function check(sources, license) {
+        var minLines = Math.min(sources.length, license.length);
+        var matchRates = new Array(minLines).reduce(function (prev, curre, index) {
+            if (license[license.length - 1] === '' && index === license.length - 1) {
+                return ++prev;
+            } else {
+                return prev + matchStrings(license[index], sources[index]);
+            }
+        });
+        return matchRates / minLines;
+    }
+
+    function processFile(file, encoding, callback) {
         var filename = getFileName(file);
         var template = gutil.template(license, extend({
             file: file, filename: filename
         }, config));
 
-        if (config.check)
-        {
+        if (config.check) {
+            this.check(file.contents.toString('utf-8').split(/\r?\n/), template.split(/\r?\n/));
             this.emit.bind(this, 'error');
         }
-        if (config.format)
-        {
+        if (config.format) {
 
         }
-        if (config.remove)
-        {
+        if (config.remove) {
 
 
         }
-    };
+    }
+
     return through.obj(processFile);
 };
